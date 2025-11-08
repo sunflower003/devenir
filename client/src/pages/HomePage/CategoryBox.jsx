80996cac542d7e7712336f18c65aba7ca45aa7dc
lehyyy <100980913+sunflower003@users.noreply.github.com>
2025-11-08 23:24:56 +0700

    form category box

--- client/src/pages/HomePage/CategoryBox.jsx
+++ client/src/pages/HomePage/CategoryBox.jsx
@@ -2,8 +2,68 @@ import styles from './CategoryBox.module.css';
 
 const CategoryBox = () => {
     return (
-        <div className={`${styles.categoryBox} container`}>
-            <h1>Category Title</h1>
+        <div className={`${styles.categoryBox}`}>
+            <div className={`${styles.box1} ${styles.box}`} style={{ 
+                backgroundImage: `url('/images/category1.png')`
+            }}>
+                <div className={styles.contentBox}>
+                    <h2 className={styles.title}>New In Jackets</h2> 
+                    <p className={styles.description}>Discover timeless craftsmanship and modern silhouettes for the season ahead.</p>
+                    <a className={styles.link} href="#">Shop</a>   
+                </div>       
+            </div>
+            <div className={`${styles.box2} ${styles.box}`} style={{ 
+                backgroundImage: `url('/images/category2.png')`
+            }}>
+                <div className={styles.contentBox}>
+                    <h2 className={styles.title}>New In Jackets</h2> 
+                    <p className={styles.description}>Discover timeless craftsmanship and modern silhouettes for the season ahead.</p>
+                    <a className={styles.link} href="#">Shop</a>   
+                </div>       
+            </div>
+            <div className={`${styles.box3} ${styles.box}`} style={{ 
+                backgroundImage: `url('/images/category3.png')`
+            }}>
+                <div className={styles.contentBox}>
+                    <h2 className={styles.title}>New In Sweaters</h2> 
+                    <p className={styles.description}>Discover timeless craftsmanship and modern silhouettes for the season ahead.</p>
+                    <a className={styles.link} href="#">Shop</a>   
+                </div>       
+            </div>
+            <div className={`${styles.box4} ${styles.box}`}>
+                <div className={styles.productSliders}>
+                    <img src="/images/prd1.png" alt="Product 1"/>
+                    <img src="/images/prd2.png" alt="Product 2"/>
+                    <img src="/images/prd3.png" alt="Product 3"/>
+                    <div className={styles.arrows}>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" fill="none">
+                            <path d="M29.7565 7.29374L13.5627 23.9062C13.2783 24.198 13.1191 24.5894 13.1191 24.9969C13.1191 25.4043 13.2783 25.7957 13.5627 26.0875L29.7565 42.7062C29.889 42.8423 30.0474 42.9505 30.2224 43.0243C30.3973 43.0982 30.5853 43.1362 30.7752 43.1362C30.9652 43.1362 31.1532 43.0982 31.3281 43.0243C31.5031 42.9505 31.6615 42.8423 31.794 42.7062C32.0666 42.4272 32.2192 42.0526 32.2192 41.6625C32.2192 41.2724 32.0666 40.8978 31.794 40.6187L16.5659 24.9969L31.794 9.37811C32.0656 9.09921 32.2177 8.72526 32.2177 8.33593C32.2177 7.94659 32.0656 7.57264 31.794 7.29374C31.6615 7.15767 31.5031 7.04951 31.3281 6.97566C31.1532 6.90182 30.9652 6.86377 30.7752 6.86377C30.5853 6.86377 30.3973 6.90182 30.2224 6.97566C30.0474 7.04951 29.889 7.15767 29.7565 7.29374Z" fill="white"/>
+                        </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" fill="none">
+                            <path d="M16.644 7.29061C16.3714 7.56966 16.2188 7.94427 16.2188 8.33436C16.2188 8.72446 16.3714 9.09907 16.644 9.37811L31.8721 25L16.644 40.6187C16.3714 40.8978 16.2188 41.2724 16.2188 41.6625C16.2187 42.0526 16.3714 42.4272 16.644 42.7062C16.7764 42.8423 16.9348 42.9505 17.1098 43.0243C17.2848 43.0982 17.4728 43.1362 17.6627 43.1362C17.8526 43.1362 18.0406 43.0982 18.2156 43.0243C18.3906 42.9505 18.549 42.8423 18.6815 42.7062L34.8752 26.0906C35.1596 25.7988 35.3188 25.4075 35.3188 25C35.3188 24.5925 35.1596 24.2012 34.8752 23.9094L18.6815 7.29374C18.549 7.15767 18.3906 7.04951 18.2156 6.97566C18.0406 6.90182 17.8526 6.86377 17.6627 6.86377C17.4728 6.86377 17.2848 6.90182 17.1098 6.97566C16.9348 7.04951 16.7764 7.15767 16.644 7.29374V7.29061Z" fill="white"/>
+                        </svg>
+                    </div>
+                </div>
+                <div className={styles.navigation}>
+                    <span className={styles.active}></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <div className={`${styles.contentBox} ${styles.box4Content}`}>
+                    <h2 className={styles.title}>For Every Adventure</h2> 
+                    <p className={styles.description}>Elevate your wardrobe with classic coats and jackets in seasonal tones and the unmistakable Burberry Check. Discover stylish solutions for the coming months, including warm puffers, quilted styles and relaxed bomber jackets.</p>
+                    <a className={styles.link} href="#">Shop Now</a>   
+                </div>   
+            </div>
+            <div className={`${styles.box5} ${styles.box}`} style={{ 
+                backgroundImage: `url('/images/category5.png')`
+            }}>
+                <div className={styles.contentBox}>
+                    <h2 className={styles.title}>The Gift Guide</h2> 
+                    <p className={styles.description}>A curated selection of timeless gifts from the World of Ralph Lauren to celebrate the season</p>
+                    <a className={styles.link} href="#">Find a Gift</a>   
+                </div>       
+            </div>
         </div>
     );
 };
--- client/src/pages/HomePage/SmallTreasures.jsx
+++ client/src/pages/HomePage/SmallTreasures.jsx
@@ -8,7 +8,7 @@ import { SplitText } from "gsap/SplitText";
 gsap.registerPlugin(useGSAP,ScrollTrigger,SplitText);
 
 const SmallTreasures = () => {
-
+    const titleRef = useRef(null);
     const smallTreasureRef = useRef(null);
 
     useGSAP(() => {
@@ -16,6 +16,16 @@ const SmallTreasures = () => {
         if (!container) return;
 
         const images = gsap.utils.toArray(`.${styles.imageParallax}`);
+
+        gsap.from(titleRef.current, {
+            opacity: 0,
+            duration: 1,
+            scrollTrigger: {
+                trigger: container,
+                start: "top 50%",
+                end: "bottom top",
+            }
+        })
 
         images.forEach((image, index) => {
             const speeds = [-300, -350, -280, -320, -290, -330, -200];
@@ -38,10 +48,30 @@ const SmallTreasures = () => {
 
     return (
         <div className={`${styles.smallTreasures}`} ref={smallTreasureRef}>
-            <div className={styles.title}>
+            <div className={styles.title} ref={titleRef}>
                 <h1>small</h1>
                 <h1>treasures</h1>
             </div>
+            <button className="button--surtur">
+                <svg className="textcircle" viewBox="0 0 500 500">
+                    <title>Projects & client work 2025</title>
+                    <defs>
+                        <path id="textcircle" d="M250,400 a150,150 0 0,1 0,-300a150,150 0 0,1 0,300Z" />
+                    </defs>
+                    <text>
+                        <textPath xlinkHref="#textcircle" aria-label="Projects & client work 2025" textLength="900">
+                            Projects &amp; client work 2025
+                        </textPath>
+                    </text>
+                </svg>
+                <svg aria-hidden="true" className="eye" width="70" height="70" viewBox="0 0 70 70" xmlns="http://www.w3.org/2000/svg">
+                    <path className="eye__outer" d="M10.5 35.308c5.227-7.98 14.248-13.252 24.5-13.252s19.273 5.271 24.5 13.252c-5.227 7.98-14.248 13.253-24.5 13.253s-19.273-5.272-24.5-13.253z"/>
+                    <path className="eye__lashes-up" d="M35 8.802v8.836M49.537 11.383l-3.31 8.192M20.522 11.684l3.31 8.192" />
+                    <path className="eye__lashes-down" d="M35 61.818v-8.836 8.836zM49.537 59.237l-3.31-8.193 3.31 8.193zM20.522 58.936l3.31-8.193-3.31 8.193z" />
+                    <circle className="eye__iris" cx="35" cy="35.31" r="5.221" />
+                    <circle className="eye__inner" cx="35" cy="35.31" r="10.041" />
+                </svg>
+            </button>
             <div className={styles.imageParallaxContainer}>
                 <img className={`${styles.imageParallax} ${styles.image1}`} src="/images/treasure1.png" />
                 <img className={`${styles.imageParallax} ${styles.image2}`} src="/images/treasure2.png" />
